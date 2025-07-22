27879f2986ed12252d232fb1e8b327510dcf82d5
Davi <davifssoares2002@gmail.com>
2025-07-21 23:24:01 -0300

    feat: saving embeddings on json

--- nearest_neighbors/create_embedding.js
+++ nearest_neighbors/create_embedding.js
@@ -3,10 +3,30 @@ import fs from "fs";
 
 const imgEmbedder = await pipeline("image-feature-extraction", "Xenova/clip-vit-base-patch32", {dtype: "fp32"});
 
-async function embedImg(imgs){
-    const embed = imgEmbedder(imgs, { pool: "cls", normalize: true })
-
-    return embed.then(t => t.tolist());
+async function embedImg(img) {
+    return imgEmbedder(img, { pooling: "cls", normalize: true }).then(t => t.tolist());
 }
 
 const images = fs.readdirSync("./train").map(f => "./train/" + f);
+
+let startIndex = 0;
+
+while (startIndex < images.length){
+    let endIndex = startIndex + 500;
+
+    let imgsToEmbed = images.slice(startIndex, endIndex);
+
+    const embeddings = await embedImg(imgsToEmbed);
+    const output = []
+
+    for(let i = 0; i < embeddings.length; i++){
+        output.push({
+            "path": images[i + startIndex],
+            "embedding": embeddings[i]
+        })
+    }
+
+    fs.writeFileSync(`embeddings/embedding_${startIndex}.json`, JSON.stringify(output))
+
+    startIndex = endIndex;
+}
